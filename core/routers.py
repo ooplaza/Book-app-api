501afe6b1bb821cf0a16b4cf97753c0eb62c4170
orlyplaza <orly.plaza@valens-research.com>
2023-10-19 03:59:24 +0800

    Added book routes

---
+++ core/routers.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import SimpleRouter
+from core.book.viewsets import BookViewSet
+
+# Instantiate
+router = SimpleRouter()
+router.register(r"books", BookViewSet, basename='book-list')
+
+# Viewsets
+
+urlpatterns = [
+    *router.urls
+]
