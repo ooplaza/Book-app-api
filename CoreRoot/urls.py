004a6fdc8f633cfb9a7d34299e5247f97fa17c3a
orlyplaza <orly.plaza@valens-research.com>
2023-10-20 01:44:11 +0800

    Added docs routes

--- CoreRoot/urls.py
+++ CoreRoot/urls.py
@@ -19,10 +19,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("core.routers"))
+    path('api/', include("core.routers")),
+    path('api/schema', SpectacularAPIView.as_view(), name="schema"),
+    path('api/schema/docs', SpectacularSwaggerView.as_view(url_name="schema")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
