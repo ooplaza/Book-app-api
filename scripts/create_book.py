85563909924644241e649cc1165dcd2533f9ead5
orlyplaza <orly.plaza@valens-research.com>
2023-10-21 16:06:12 +0800

    Change for loop design

--- scripts/create_book.py
+++ scripts/create_book.py
@@ -11,29 +11,21 @@ def run():
     """
     datasets = pd.read_csv("./Datasets/Books.csv", low_memory=False)
     with transaction.atomic():
-        bulk_books = [
-            Book(
-                ISBN=ISBN,
-                title=TITLE,
-                release_year=RELEASE_YEAR,
-                publisher=PUBLISHER,
-                thumbnail=THUMBNAIL,
-                ratings=RATINGS,
-                price=PRICE,
-                author=AUTHOR,
-            )
-            for ISBN, TITLE, RELEASE_YEAR, PUBLISHER, THUMBNAIL, RATINGS, PRICE, AUTHOR
-            in zip(datasets["ISBN"],
-                   datasets["TITLE"],
-                   datasets["RELEASE_YEAR"],
-                   datasets["PUBLISHER"],
-                   datasets["THUMBNAIL"],
-                   datasets["RATINGS"],
-                   datasets["PRICE"],
-                   datasets["AUTHOR"],
-                   )
-        ]
+        counter = 0
+        bulk_books = []
+        for ISBN, TITLE, RELEASE_YEAR, PUBLISHER, THUMBNAIL, RATINGS, PRICE, AUTHOR in zip(datasets["ISBN"],
+                                                                                           datasets["TITLE"],
+                                                                                           datasets["RELEASE_YEAR"],
+                                                                                           datasets["PUBLISHER"],
+                                                                                           datasets["THUMBNAIL"],
+                                                                                           datasets["RATINGS"],
+                                                                                           datasets["PRICE"],
+                                                                                           datasets["AUTHOR"]):
+            counter += 1
+            print(f"{counter} - {TITLE}")
+            bulk_books.append(Book(ISBN=ISBN, title=TITLE, release_year=RELEASE_YEAR, publisher=PUBLISHER,
+                                   thumbnail=THUMBNAIL, ratings=RATINGS, price=PRICE, author=AUTHOR))
 
         # Create Bulk_Books
         Book.objects.bulk_create(bulk_books, ignore_conflicts=False, batch_size=1000)
-    print("Done!")
+    print("Finished!")
