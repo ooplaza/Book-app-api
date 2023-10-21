11cf44b04776e7661ba68d31a00ded3918d73945
orlyplaza <orly.plaza@valens-research.com>
2023-10-21 16:17:01 +0800

    Added production settings

--- CoreRoot/wsgi.py
+++ CoreRoot/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoreRoot.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+# Vercel Configuration
+app = get_wsgi_application()
