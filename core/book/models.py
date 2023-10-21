0314ae4a7a5dbea619bd8b093ed45366741fff9e
orlyplaza <orly.plaza@valens-research.com>
2023-10-21 16:06:47 +0800

    Changed max_length size

--- core/book/models.py
+++ core/book/models.py
@@ -5,13 +5,13 @@ from django.db import models
 # Create your models here.
 class Book(AbstractModel):
     ISBN = models.CharField(max_length=20)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=500)
     release_year = models.IntegerField(default=0)
-    publisher = models.CharField(max_length=255, default='Unknown')
+    publisher = models.CharField(max_length=500, default='Unknown')
     thumbnail = models.URLField()
     ratings = models.FloatField(default=0.0)
     price = models.FloatField(default=0.0)
-    author = models.CharField(max_length=255)
+    author = models.CharField(max_length=500)
 
     def __str__(self):
         return f"{self.title}"
